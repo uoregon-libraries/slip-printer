60fc369964206055c43e0f8fa9301bbf9a3dbe2a
Jeremy Echols <jechols@uoregon.edu>
2021-11-19 07:22:53 -0800

    Add insane levels of error checking for curl & xml

--- functions.php
+++ functions.php
@@ -5,14 +5,43 @@ require_once('vars.php');
 // GET or POST to API
 function get_result($url, $type="get") {
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  if ($type == 'post') {
-    curl_setopt($ch, CURLOPT_POST, 1);
+  if ($ch === FALSE) {
+    error_log("Unable to initialize curl: " . curl_error($ch));
+    die("Error trying to pull data from Ex Libris. Try again or contact support.");
   }
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+  if (curl_setopt($ch, CURLOPT_URL, $url) === FALSE) {
+    error_log("Unable to set URL on curl: " . curl_error($ch));
+    die("Error trying to pull data from Ex Libris. Try again or contact support.");
+  }
+  if ($type == 'post') {
+    if (curl_setopt($ch, CURLOPT_POST, 1) === FALSE) {
+      error_log("Unable to set method to POST on curl: " . curl_error($ch));
+      die("Error trying to pull data from Ex Libris. Try again or contact support.");
+    }
+  }
+  if (curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1) === FALSE) {
+      error_log("Unable to set return transfer to 1 on curl: " . curl_error($ch));
+      die("Error trying to pull data from Ex Libris. Try again or contact support.");
+  }
+
   $output = curl_exec($ch);
+  if ($output === FALSE) {
+    error_log("Unable to execute curl operation: " . curl_error($ch));
+    die("Error trying to pull data from Ex Libris. Try again or contact support.");
+  }
   curl_close($ch);
-  return simplexml_load_string($output);
+
+  libxml_use_internal_errors(true);
+  $data = simplexml_load_string($output);
+  if ($data === FALSE) {
+    error_log("Unable to parse XML");
+    foreach(libxml_get_errors() as $error) {
+        error_log("--> XML error: " . $error->message);
+    }
+    die("Error trying to pull data from Ex Libris. Try again or contact support.");
+  }
+  return $data;
 }
 
 // Get printers
